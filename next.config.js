b4c98406939a73c2ab87cbf92dbeae3c9c826bb4
qudusayo <qqudusayo@gmail.com>
2023-07-13 14:41:12 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: ["mui-one-time-password-input"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
